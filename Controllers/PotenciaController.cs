a250182abd18d42ba89d732b19e52d40bd28ac11
Hector Sanchez <20186183@itla.edu.do>
2020-02-17 06:09:03 -0400

    Nuevas Funciones

---
+++ Controllers/PotenciaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Calculadora_Web_Ejercicio_4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculadora_Web_Ejercicio_4.Controllers
+{
+    public class PotenciaController : Controller
+    {
+        public IActionResult PotenciaView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PotenciaView(Operaciones operaciones, string Funtion)
+        {
+            Double Total = 0;
+            Double V1 = operaciones.Valor1;
+            Double V2 = operaciones.Valor2;
+            Total = Math.Pow(V1, V2);
+
+            ViewData["Resultado"] = Total;
+            return View();
+        }
+    }
+}
---
+++ Controllers/RaicesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Calculadora_Web_Ejercicio_4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculadora_Web_Ejercicio_4.Controllers
+{
+    public class RaicesController : Controller
+    {
+        public IActionResult Raices()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Raices(Operaciones operaciones, string Funtion)
+        {
+            Double Total = 0;
+            Double V1 = operaciones.Valor1;
+
+            switch (Funtion)
+            {
+                case "sqrt":
+                    Total = Math.Sqrt(V1);
+                    break;
+
+                case "cbrt":
+                    Total = Math.Cbrt(V1);
+                    break;
+                default:
+                    Total = 0;
+                    break;
+            }
+            Total = (Math.Round((double)Total * 10000)) / 10000;
+            ViewData["Resultado"] = Total;
+            return View();
+        }
+    }
+}
