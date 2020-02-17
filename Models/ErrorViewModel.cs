3b8b2b2230551b788a9fdde57bcd09c3426041e6
Hector Sanchez <20186183@itla.edu.do>
2020-02-16 22:40:39 -0400

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Calculadora_Web_Ejercicio_4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
